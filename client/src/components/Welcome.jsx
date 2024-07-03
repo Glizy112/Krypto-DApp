7ac26edd34040f264a8eafd6c0908a7e4fa75c50
Glizy112 <77720813+Glizy112@users.noreply.github.com>
2024-07-03 22:30:11 +0530

    Minor Change

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -44,7 +44,7 @@ const Welcome =()=> {
             <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1"> 
-                        Send Crypto <br/> across everywhere
+                        Send Crypto <br/> across everywher
                     </h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto revolution. Send money your way easily on Krypto.
